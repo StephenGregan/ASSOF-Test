3c4bf2ba5573e74dd422dcc19ae4a22e21b43dfd
StephenGregan <grego@live.ie>
2018-06-06 14:06:57 +0100

    update

--- ConsoleAppASSOFTest/Program.cs
+++ ConsoleAppASSOFTest/Program.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +15,47 @@ namespace ConsoleAppASSOFTest
     {
         static void Main(string[] args)
         {
-         =
+            string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
+            string adminApiKey = ConfigurationManager.AppSettings["SearchServiceAdminApiKey"];
+            SearchServiceClient serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(adminApiKey));
+
+            var dataSource = DataSource.AzureBlobStorage("ronanblobstorage", "DefaultEndpointsProtocol=https;AccountName=ronanblobstorage;AccountKey=Z8nPhGaeduop96WgvZ2FP6QKGNjXeN/G8RmyoJtX97Cycusq5WOaIymucItSNnrv31ChYMICh04nmyHbCmySYw==;EndpointSuffix=core.windows.net", "contactjson");
+            //create data source
+            if (serviceClient.DataSources.Exists(dataSource.Name))
+            {
+                serviceClient.DataSources.Delete(dataSource.Name);
+            }
+
+            serviceClient.DataSources.Create(dataSource);
+
+            var definition = new Index()
+            {
+                Name = "stephensindex",
+                Fields = FieldBuilder.BuildForType<StephenTestModel>()
+            };
+            //create Index
+            if (serviceClient.Indexes.Exists(definition.Name))
+            {
+                serviceClient.Indexes.Delete(definition.Name);
+            }
+            var index = serviceClient.Indexes.Create(definition);
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=ronanblobstorage;AccountKey=Z8nPhGaeduop96WgvZ2FP6QKGNjXeN/G8RmyoJtX97Cycusq5WOaIymucItSNnrv31ChYMICh04nmyHbCmySYw==;EndpointSuffix=core.windows.net");
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference("contactjson");
+            var blobList = container.ListBlobs();
+
+            var stephensIndexsList = blobList.Select(blob => new StephenTestModel
+            {
+                fileId = Guid.NewGuid().ToString(),
+                blobURL = blob.Uri.ToString(),
+                fileText = "Blob Content",
+                keyPhrases = "key phrases",
+            }).ToList();
+            var batch = IndexBatch.Upload(stephensIndexsList);
+            ISearchIndexClient indexClient = serviceClient.Indexes.GetClient("index");
+            indexClient.Documents.Index(batch);
         }
     }
 }
+
--- ConsoleAppASSOFTest/StephenTestModel.cs
+++ ConsoleAppASSOFTest/StephenTestModel.cs
@@ -11,45 +11,22 @@ using System.Configuration;
 using System.Configuration.Assemblies;
 using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Azure.Search.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ConsoleAppASSOFTest
 {
+    [SerializePropertyNamesAsCamelCase]
     public class StephenTestModel
     {
         [System.ComponentModel.DataAnnotations.Key]
         [IsFilterable]
-        public string fieldId { get; set; }
+        public string fileId { get; set; }
         [IsSearchable]
         public string fileText { get; set; }
 
-        public string blobURl { get; set; }
+        public string blobURL { get; set; }
         [IsSearchable]
-        public string keyPhrase { get; set; }
+        public string keyPhrases { get; set; }
 
-        string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
-        string adminApiKey = ConfigurationManager.AppSettings["SearchServiceAdminApiKey"];
-        SearchServiceClient serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(adminApiKey));
-
-        var dataSource = DataSource.AzureBlobStorage("storage name", "connectstrong", "container name");
-        //create data source
-        if (serviceClient.DataSources.Exists(dataSource.Name))
-        {
-            serviceClient.DataSources.Delete(dataSource.Name);
-        }
-
-        serviceClient.DataSources.Create(dataSource);
-
-
-        var definition = new Index()
-        {
-            Name = "stephensindex",
-            Fields = FieldBuilder.BuildForType<StephenTestModel>()
-        };
-    //create Index
-    if (serviceClient.Indexes.Exists(definition.Name))
-    {
-        serviceClient.Indexes.Delete(definition.Name);
-    }
-var index = serviceClient.Indexes.Create(definition);
     }
 }
